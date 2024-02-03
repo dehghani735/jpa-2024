2aa92cc81d3b1f7554bd344c2e3b5d92dafa75cb
mr.tafti <dehghani735@gmail.com>
2024-02-03 18:15:26 +0330

    lesson 3 adding.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,77 @@ import java.util.Map;
 
 public class Main {
 
+//    public static void main_e1(String[] args) {
+//
+//        String puName = "my-persistence-unit";
+//        Map<String, String> props = new HashMap<>();
+//        props.put("hibernate.show_sql", "true");
+//        props.put("hibernate.hbm2ddl.auto", "create"); // create, update, none
+//
+//        EntityManagerFactory emf = new HibernatePersistenceProvider()
+//                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
+////        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit"); // factory pattern design object
+//        EntityManager em = emf.createEntityManager(); // represents the context
+//
+//        try {
+//            em.getTransaction().begin();
+//
+////            Product p = new Product();
+////            p.setId(1L);
+////            p.setName("Beer");
+////
+////            em.persist(p); // add this to the context -> Not an insert query
+//
+//            //
+//
+////            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
+////            e1.setName("John Doe");
+////            e1.setName("Laurentiu Spi");   // there will be no update. because nothing has changed.
+////
+////            System.out.println(e1);
+//
+//            //
+//            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
+//            em.remove(e1); // remove instance from the context
+//
+////            em.persist(); // -> Adding an entity to the context.
+////            em.find();    // -> Finds by PK. Get from DB and add it to the context if it doesn't already exist.
+////            em.remove();  // -> Marking entity for removal.
+////            em.merge();   // -> Merges an entity from outside the context to the context.
+////            em.refresh(); // -> Mirror the context from the database.
+////            em.detach();  // -> taking the entity out of the context.
+//
+////            Employee e2 = new Employee();
+////            e2.setId(1);
+////            e2.setName("Laur");
+////            e2.setAddress("123 Main St.");
+////
+////            em.persist(e2);
+//
+//            //
+//
+////            Employee e3 = new Employee();
+////            e3.setId(1);
+////            e3.setName("John Doe");
+////            e3.setAddress("Something");
+////            em.merge(e3);
+//
+//            //
+//
+//
+//
+//            em.getTransaction().commit(); // end of transaction
+//        } finally {
+//            em.close();
+//        }
+//    }
+
     public static void main(String[] args) {
 
         String puName = "my-persistence-unit";
-        Map<?, ?> props = new HashMap<>();
+        Map<String, String> props = new HashMap<>();
+        props.put("hibernate.show_sql", "true");
+        props.put("hibernate.hbm2ddl.auto", "update"); // create, update, none
 
         EntityManagerFactory emf = new HibernatePersistenceProvider()
                 .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
@@ -24,51 +91,24 @@ public class Main {
         try {
             em.getTransaction().begin();
 
-//            Product p = new Product();
-//            p.setId(1L);
-//            p.setName("Beer");
+//            var e1 = new Employee();
+//            e1.setId(1);
+//            e1.setName("John");
+//            e1.setAddress("Address");
 //
-//            em.persist(p); // add this to the context -> Not an insert query
-
-            //
-
-//            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
-//            e1.setName("John Doe");
-//            e1.setName("Laurentiu Spi");   // there will be no update. because nothing has changed.
+//            em.persist(e1);
 //
+//            e1 = em.find(Employee.class, 1); // find does not make a select query from database. because it is already in the context
 //            System.out.println(e1);
 
-            //
-            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
-            em.remove(e1); // remove instance from the context
+            // find vs getReference
+            var e1 = em.find(Employee.class, 1); // always create select query
 
-//            em.persist(); // -> Adding an entity to the context.
-//            em.find();    // -> Finds by PK. Get from DB and add it to the context if it doesn't already exist.
-//            em.remove();  // -> Marking entity for removal.
-//            em.merge();   // -> Merges an entity from outside the context to the context.
-//            em.refresh(); // -> Mirror the context from the database.
-//            em.detach();  // -> taking the entity out of the context.
-
-//            Employee e2 = new Employee();
-//            e2.setId(1);
-//            e2.setName("Laur");
-//            e2.setAddress("123 Main St.");
-//
-//            em.persist(e2);
-
-            //
-
-//            Employee e3 = new Employee();
-//            e3.setId(1);
-//            e3.setName("John Doe");
-//            e3.setAddress("Something");
-//            em.merge(e3);
-
-            //
+            var e2 = em.getReference(Employee.class, 1); // no query is issued to the db
+            System.out.println(e2); // it is called when you do anything with the reference. it is based on decorator pattern.
 
 
-
-            em.getTransaction().commit(); // end of transaction
+            em.getTransaction().commit();
         } finally {
             em.close();
         }
