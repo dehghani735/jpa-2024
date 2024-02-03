fc75172a1c7d2486d1ebdcb581c6a31ff570fc67
mr.tafti <dehghani735@gmail.com>
2024-02-03 17:40:39 +0330

    first commit.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+<
+jpa_2023.iml,4\d\4d2e9c5c64631c6419196f1fcd48494c9454f83f
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+_
+/src/main/java/org/example/entities/Product.java,e\4\e4c0a09511c55dd3285499378bc2650c14099ab6
+t
+Dsrc/main/java/org/example/persistence/CustomPersistenceUnitInfo.java,a\9\a91ccfada52f0a80935008544ad4cda502935ce3
+`
+0src/main/java/org/example/entities/Employee.java,9\8\98dc6e23b79df12c5f871f2ad1e199c54eef56ef
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.example.entities.Employee;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String puName = "my-persistence-unit";
+        Map<?, ?> props = new HashMap<>();
+
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit"); // factory pattern design object
+        EntityManager em = emf.createEntityManager(); // represents the context
+
+        try {
+            em.getTransaction().begin();
+
+//            Product p = new Product();
+//            p.setId(1L);
+//            p.setName("Beer");
+//
+//            em.persist(p); // add this to the context -> Not an insert query
+
+            //
+
+//            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
+//            e1.setName("John Doe");
+//            e1.setName("Laurentiu Spi");   // there will be no update. because nothing has changed.
+//
+//            System.out.println(e1);
+
+            //
+            var e1 = em.find(Employee.class, 1); // Laurentiu Spi
+            em.remove(e1); // remove instance from the context
+
+//            em.persist(); // -> Adding an entity to the context.
+//            em.find();    // -> Finds by PK. Get from DB and add it to the context if it doesn't already exist.
+//            em.remove();  // -> Marking entity for removal.
+//            em.merge();   // -> Merges an entity from outside the context to the context.
+//            em.refresh(); // -> Mirror the context from the database.
+//            em.detach();  // -> taking the entity out of the context.
+
+//            Employee e2 = new Employee();
+//            e2.setId(1);
+//            e2.setName("Laur");
+//            e2.setAddress("123 Main St.");
+//
+//            em.persist(e2);
+
+            //
+
+//            Employee e3 = new Employee();
+//            e3.setId(1);
+//            e3.setName("John Doe");
+//            e3.setAddress("Something");
+//            em.merge(e3);
+
+            //
+
+
+
+            em.getTransaction().commit(); // end of transaction
+        } finally {
+            em.close();
+        }
+    }
+}
