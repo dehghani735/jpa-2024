ccd17255110f19d7dc1c349392f6e83f7b6abcb5
mr.detafti <dehghani735@gmail.com>
2024-02-08 16:10:36 +0330

    Polished.

---
+++ src/main/java/org/example/entities/Course.java
@@ -0,0 +1,52 @@
+package org.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+public class Course {
+
+    @Id
+    private long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "course")
+    private List<Enrollment> enrollments;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+
+    public void setEnrollments(List<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", enrollments=" + enrollments +
+               '}';
+    }
+}
