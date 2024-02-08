96a5c3f123abe2950e724a989b1430f0e1fa41b4
mr.detafti <dehghani735@gmail.com>
2024-02-08 16:03:44 +0330

    query added.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import org.example.entities.Student;
-import org.example.entities.keys.StudentKey;
+import jakarta.persistence.TypedQuery;
 import org.example.persistence.CustomPersistenceUnitInfo;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
@@ -181,8 +180,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String puName = "my-persistence-unit";
+        Map<String, String> props = new HashMap<>();
+        props.put("hibernate.show_sql", "true");
+        props.put("hibernate.hbm2ddl.auto", "create"); // create, update, none
 
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit"); // factory pattern design object
+        EntityManager em = emf.createEntityManager(); // represents the context
 
+        try {
+            em.getTransaction().begin();
+
+            String jpql7 = """
+                    SELECT NEW org.example.dto.EnrolledStudent(s, (SELECT count(e) FROM Enrollment e where e.student = s) ) 
+                    From Student s
+                    """;
+            TypedQuery<CountedEnrollmentStudent> q2 = em.createQuery(jpql7, CountedEnrollmentStudent.class);
+            q2.getResultStream().forEach(o -> System.out.println(o.s() + " " + o.count()));
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 
 }
